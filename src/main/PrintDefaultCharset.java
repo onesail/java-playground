afa39aefdbf57374391f3b7239d09d6d6c0e7ab9
gmuth <scm@gmuth.de>
2016-04-07 21:47:44 +0200

    first main class

---
+++ src/main/PrintDefaultCharset.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.nio.charset.Charset;
+
+public class PrintDefaultCharset {
+
+    public static void main(String[] args) {
+	    System.out.println("defaultCharset: " + Charset.defaultCharset());
+    }
+}
