cf4efd018b8df3371ae887cb53690d1b96c8e270
gmuth <scm@gmuth.de>
2016-04-07 22:02:36 +0200

    more tests

--- src/main/PrintDefaultCharset.java
+++ src/main/PrintDefaultCharset.java
@@ -1,10 +1,21 @@
 package main;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 
 public class PrintDefaultCharset {
 
     public static void main(String[] args) {
-	    System.out.println("defaultCharset: " + Charset.defaultCharset());
+        System.out.println("Default Charset            : " + Charset.defaultCharset());
+        System.out.println("Default Charset Name       : " + Charset.defaultCharset().name());
+        System.out.println("Default Charset Displayname: " + Charset.defaultCharset().displayName());
+
+        //System.setProperty("file.encoding", "Latin-1");
+        System.out.println("Property file.encoding     : " + System.getProperty("file.encoding"));
+
+        OutputStreamWriter writer = new OutputStreamWriter(new ByteArrayOutputStream());
+        System.out.println("OutputStreamWriter Encoding: " + writer.getEncoding());
     }
+
 }
